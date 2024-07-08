45fa9da96de819242c83d613880abd0c39ea65e9
Bensoli <benswambua96@gmail.com>
2024-07-08 12:43:29 +0300

    modules exports, imports

---
+++ alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['items1', 'item2'];
+const person = {
+name:'bob'
+}
+
+module.exports.singlePerson = person
--- modules.js
+++ modules.js
@@ -1,9 +1,13 @@
 //start by importing global modules exported
 const names = require('./names')
 const greeting = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind-grenade')
 
-greeting(names.bensoli)
-greeting(names.peter)
-greeting(names.john)
-greeting('alex')
+
+console.log(data)
+// greeting(names.bensoli)
+// greeting(names.peter)
+// greeting(names.john)
+// greeting('alex')
 
