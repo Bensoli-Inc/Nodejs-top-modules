3f20e7e5b684dcb88a04375d9bd673dba10125ee
Bensoli <benswambua96@gmail.com>
2024-07-09 14:27:10 +0300

    installing node modules and dependencies

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
-//WEB SERVERS KEEP ON LISTENING FOR REQUESTS
+const _ =require('lodash');
 
-const http = require('http');
-
-//using the .createserver method, assigning it to a random variable for instance the name server
-//then the create server method is looking for a callback, and the callback function takes in two parameters
-//bith params are objects, and you can give them random names but (req, res) are most preferred. req- represents
-//the incoming request(from webpage). res- is what we are senting back to the client side
-
-const server = http.createServer((req, res) => {
-    if(req.url  === '/'){ //.url method checks the url requested by client
-        res.end('Hello bensoli welcome to homepage') //.end method will end the instance of our server
-    }
-    else if(req.url === '/about'){
-        res.end('Here is my developer journey')
-    } else {
-        res.end(`
-           <h1>Ooops!</h1>
-           <p> Page not found </p>
-           <a href="/">back Home</a>
-            `);
-        }
-    })
-
-//saying what port our server will be listening to, using the.listen method
-server.listen(5000)
-console.log(`server is listening on port 5000`);
+const items = [1,[2, [3, [4]]]]
+const newItems =  _.flattenDeep(items)
+console.log(newItems)
